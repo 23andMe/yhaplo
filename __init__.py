362a81deee8ee8f24028b26ede4a1715381f9bcd
David Poznik <dpoznik@23andme.com>
2016-12-19 10:49:48 -0800

    clear __init__.py content

--- __init__.py
+++ __init__.py
@@ -1,12 +1,1 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import pkg_resources
-import warnings
-
-try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except pkg_resources.DistributionNotFound as e:  # pragma: no cover
-    warnings.warn("Version not available for package %s (is it installed?)" % __package__,
-                  Warning)
-    __version__ = None
+#
