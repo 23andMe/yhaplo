386b70099355f9096d188450c5ef923110776774
David Poznik <dpoznik@23andme.com>
2021-03-26 20:28:37 -0700

    Use entry points and pyenv in test script

--- test_github_yhaplo.sh
+++ test_github_yhaplo.sh
@@ -32,25 +32,23 @@ py3_output_dir=output.yhaplo.py3
 nwk_fn=output/y.tree.primary.aligned.ycc.2016.01.04.nwk
 tree_drawing_fn=${nwk_fn%.nwk}.drawing.txt
 
-# parameters
-python_path=/Users/${USER}/anaconda..PY_VERSION../bin/python
+py_version_start=$(pyenv global)
 
 rm -fr output
-for py_version in 2 3; do
+for py_version in 2.7.18 3.8.6; do
     echo -e "Python ${py_version}\n\n"
-    python_ex=${python_path/..PY_VERSION../${py_version}}
-    run_yhaplo="${python_ex} -m yhaplo.call_haplogroups"
+    pyenv global ${py_version}
     
     echo -e "Text Input\n"
-    ${run_yhaplo} --example_1000Y_subset --hgGenos Q
+    yhaplo --example_1000Y_subset --hgGenos Q
     
     echo -e "\n\nSingle-Sample VCF Input\n"
-    ${run_yhaplo} --example_single_sample_vcf --hgGenos Q
+    yhaplo --example_single_sample_vcf --hgGenos Q
     
     echo -e "\n\nMulti-Sample VCF Input\n"
     if [ $# == 1 ] && [ "$1" == "m" ]; then
         if [ -e ${multi_sample_vcf_fn} ]; then
-            ${run_yhaplo} -i ${multi_sample_vcf_fn} --hgGenos Q
+            yhaplo -i ${multi_sample_vcf_fn} --hgGenos Q
         else
             echo "File not found: ${multi_sample_vcf_fn}"
             echo -e "Download from:\n${multi_sample_vcf_url}"
@@ -61,7 +59,7 @@ for py_version in 2 3; do
     
     echo -e "\n\nFormat Converter\n"
     if [ -e ${ttam_data_fn} ]; then
-        ${python_ex} -m yhaplo.convert_to_genos ${ttam_data_fn}
+        convert_to_genos ${ttam_data_fn}
         mv converted/* output/
         rmdir converted
     else
@@ -69,18 +67,23 @@ for py_version in 2 3; do
     fi
     
     echo -e "\n\nTree Plotter\n"
-    ${python_ex} -m yhaplo.plot_tree -n ${nwk_fn} | tee ${tree_drawing_fn}
+    plot_tree -n ${nwk_fn} | tee ${tree_drawing_fn}
     
     echo
-    out_dir=output.yhaplo.py${py_version}
+    py_version_major=$(echo $py_version | cut -f1 -d".")
+    out_dir=output.yhaplo.py${py_version_major}
     rm -fr ${out_dir}
     mv output ${out_dir}
 done;
+pyenv global ${py_version_start}
+
 
 echo -e "\n\n\n----------------------------------------------------------------------"
 echo -e "Expected versus py2\n"
 if [ -d ${expected_output_dir} ]; then
-    ls ${expected_output_dir} | xargs -I {} echo diff ${expected_output_dir}/{} ${py2_output_dir}/{} | sh -v
+    ls ${expected_output_dir} \
+    | xargs -I {} echo diff ${expected_output_dir}/{} ${py2_output_dir}/{} \
+    | sh -v
 else
     echo "Directory not found: ${expected_output_dir}"
 fi
@@ -88,4 +91,6 @@ fi
 
 echo -e "\n\n\n----------------------------------------------------------------------"
 echo -e "Py2 versus Py3\n"
-ls ${py2_output_dir} | xargs -I {} echo diff ${py2_output_dir}/{} ${py3_output_dir}/{} | sh -v
+ls ${py2_output_dir} \
+| xargs -I {} echo diff ${py2_output_dir}/{} ${py3_output_dir}/{} \
+| sh -v
