d26fa13d023ad010b832342022c3de6ae05ba50b
David Poznik <dpoznik@23andme.com>
2024-03-20 10:31:54 -0700

    Use list comparison in test

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from yhaplo.config import Config
 from yhaplo.tree import Tree
 from yhaplo.utils.context_managers import logging_disabled
@@ -11,7 +9,8 @@ def test_hg_snp_idempotency():
         tree_1 = Tree(config)
         tree_2 = Tree(config)
 
-    hg_snps_1 = np.array([node.hg_snp for node in tree_1.depth_first_node_list])
-    hg_snps_2 = np.array([node.hg_snp for node in tree_2.depth_first_node_list])
+    assert extract_hg_snps(tree_1) == extract_hg_snps(tree_2)
 
-    assert (hg_snps_1 == hg_snps_2).all()
+
+def extract_hg_snps(tree):
+    return [node.hg_snp for node in tree.depth_first_node_list]
