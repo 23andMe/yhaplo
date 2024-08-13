f9a91f0d8b51476608aa4334bd42c7dc8dd6850c
David Poznik <dpoznik@23andme.com>
2024-08-12 17:02:27 -0700

    Set fallback version

--- yhaplo/__init__.py
+++ yhaplo/__init__.py
@@ -4,10 +4,11 @@ import warnings
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version(__package__)  # noqa
+    __version__ = version(__package__)
 except PackageNotFoundError:
     warnings.warn(
-        f"{__package__} version unavailable. Is it installed?",  # noqa
+        f"{__package__} version unavailable. Is it installed?",
         Warning,
         stacklevel=1,
     )
+    __version__ = "Unknown"
