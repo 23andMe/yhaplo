4677942eac8f41e89c61bc60db1e14c459aa8f68
David Poznik <dpoznik@23andme.com>
2025-07-21 10:29:10 -0700

    User `UserWarning` rather than `Warning`

--- yhaplo/__init__.py
+++ yhaplo/__init__.py
@@ -6,9 +6,5 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version(__package__)
 except PackageNotFoundError:
-    warnings.warn(
-        f"{__package__} version unavailable. Is it installed?",
-        Warning,
-        stacklevel=1,
-    )
+    warnings.warn(f"{__package__} version unavailable. Is it installed?", stacklevel=1)
     __version__ = "Unknown"
